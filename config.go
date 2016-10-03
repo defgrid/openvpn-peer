a3ae0db2338ba1c226c32a6c4516f96acdaab61d
Martin Atkins <mart@degeneration.co.uk>
2016-10-02 20:02:26 -0700

    Support for configuration from a file

---
+++ config.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/hashicorp/hcl"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	NodeName             string   `hcl:"node_name" envconfig:"OPENVPN_PEER_NODE_NAME"`
+	LocalInterface       string   `hcl:"local_interface" envconfig:"OPENVPN_PEER_INTERFACE"`
+	RegionPrefixLen      int      `hcl:"region_prefix_length" envconfig:"OPENVPN_PEER_REGION_PREFIX_LEN"`
+	DCPrefixLen          int      `hcl:"datacenter_prefix_length" envconfig:"OPENVPN_PEER_DC_PREFIX_LEN"`
+	PublicIPAddress      string   `hcl:"public_ip_address" envconfig:"OPENVPN_PEER_PUBLIC_IP"`
+	VPNEndpointStartPort int      `hcl:"vpn_endpoint_start_port" envconfig:"OPENVPN_PEER_START_PORT"`
+	GossipPort           int      `hcl:"gossip_port" envconfig:"OPENVPN_PEER_GOSSIP_PORT"`
+	GossipEncryptionKey  string   `hcl:"gossip_encryption_key" envconfig:"OPENVPN_PEER_GOSSIP_KEY"`
+	DataDir              string   `hcl:"data_dir" envconfig:"OPENVPN_PEER_DATA_DIR"`
+	InitialPeers         []string `hcl:"initial_peers"`
+}
+
+func ConfigFromFile(filename string) (*Config, error) {
+	sourceBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error reading %s: %s", filename, err)
+	}
+
+	ret := &Config{}
+	err = hcl.Unmarshal(sourceBytes, ret)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing %s: %s", filename, err)
+	}
+
+	return ret, nil
+}
+
+func ConfigFromEnv() (*Config, error) {
+	ret := &Config{}
+	err := envconfig.Process("openvpn-peer", ret)
+	return ret, err
+}
+
+func LoadConfig(filename string) (*Config, error) {
+	envCfg, err := ConfigFromEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	fileCfg, err := ConfigFromFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	envCfg.Override(fileCfg)
+	return envCfg, nil
+}
+
+func (c *Config) Override(other *Config) {
+	if other.NodeName != "" {
+		c.NodeName = other.NodeName
+	}
+	if other.LocalInterface != "" {
+		c.LocalInterface = other.LocalInterface
+	}
+	if other.RegionPrefixLen != 0 {
+		c.RegionPrefixLen = other.RegionPrefixLen
+	}
+	if other.DCPrefixLen != 0 {
+		c.DCPrefixLen = other.DCPrefixLen
+	}
+	if other.PublicIPAddress != "" {
+		c.PublicIPAddress = other.PublicIPAddress
+	}
+	if other.VPNEndpointStartPort != 0 {
+		c.VPNEndpointStartPort = other.VPNEndpointStartPort
+	}
+	if other.GossipPort != 0 {
+		c.GossipPort = other.GossipPort
+	}
+	if other.GossipEncryptionKey != "" {
+		c.GossipEncryptionKey = other.GossipEncryptionKey
+	}
+	if other.DataDir != "" {
+		c.DataDir = other.DataDir
+	}
+	if other.InitialPeers != nil && len(other.InitialPeers) > 0 {
+		c.InitialPeers = other.InitialPeers
+	}
+}
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"net"
 	"os"
-	"strconv"
 	"text/tabwriter"
 	"time"
 
@@ -15,26 +16,81 @@ import (
 func main() {
 	// Initial prototype of using serf, to start.
 
-	startJoin := []string{"127.0.0.1:9020", "127.0.0.1:9021"}
-	serfPort, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		panic(err)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 1 {
+		fmt.Fprintf(os.Stderr, "Usage: openvpn-peer [config-file]\n")
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "All settings may also be set via environment variables.\n\n")
+		os.Exit(2)
 	}
+	var config *Config
+	var err error
+	if len(args) == 1 {
+		config, err = ConfigFromFile(args[0])
+	} else {
+		config, err = ConfigFromEnv()
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(2)
+	}
+
+	log.Printf("Config is %#v", config)
 
 	serfConfig := serf.DefaultConfig()
 	serfConfig.MemberlistConfig = memberlist.DefaultWANConfig()
 
-	serfConfig.MemberlistConfig.BindAddr = "127.0.0.1"
-	serfConfig.MemberlistConfig.BindPort = serfPort
-	serfConfig.MemberlistConfig.AdvertiseAddr = "127.0.0.1"
-	serfConfig.MemberlistConfig.AdvertisePort = serfPort
-	serfConfig.NodeName = fmt.Sprintf("node-%d", serfPort)
-	serfConfig.Tags = map[string]string{
-		"region":     fmt.Sprintf("fake-region-%d", serfPort),
-		"datacenter": fmt.Sprintf("fake-region-%da", serfPort),
-		"prefix":     fmt.Sprintf("10.%d.0.0/16/24", serfPort-9000),
+	iface, err := net.InterfaceByName(config.LocalInterface)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read %s interface config: %s\n", config.LocalInterface, err)
+		os.Exit(2)
 	}
-	serfConfig.SnapshotPath = fmt.Sprintf("snap-%d", serfPort)
+
+	localAddrs, err := iface.Addrs()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to enumerate addresses for %s: %s\n", config.LocalInterface, err)
+		os.Exit(2)
+	}
+	if len(localAddrs) == 0 {
+		fmt.Fprintf(os.Stderr, "%s has no addresses: %s\n", config.LocalInterface, err)
+		os.Exit(2)
+	}
+
+	ipv4AddrCount := 0
+	var localAddr string
+	for _, addr := range localAddrs {
+		if ipNet, ok := addr.(*net.IPNet); ok {
+			ipv4Addr := ipNet.IP.To4()
+			if ipv4Addr == nil {
+				continue
+			}
+			if localAddr == "" {
+				localAddr = ipv4Addr.String()
+			}
+			ipv4AddrCount = ipv4AddrCount + 1
+		}
+	}
+
+	if ipv4AddrCount == 0 {
+		fmt.Fprintf(os.Stderr, "%s has no IPv4 addresses: %s\n", config.LocalInterface, err)
+		os.Exit(2)
+	}
+
+	log.Printf("Local IP address is %#s", localAddr)
+	if ipv4AddrCount > 1 {
+		log.Printf("%s has multiple IPv4 addresses, so I just picked one arbitrarily", config.LocalInterface)
+	}
+
+	serfConfig.MemberlistConfig.BindAddr = localAddr
+	serfConfig.MemberlistConfig.BindPort = config.GossipPort
+	serfConfig.MemberlistConfig.AdvertiseAddr = config.PublicIPAddress
+	serfConfig.MemberlistConfig.AdvertisePort = config.GossipPort
+	serfConfig.NodeName = config.NodeName
+	serfConfig.Tags = map[string]string{
+		"int_ip": localAddr,
+	}
+	serfConfig.SnapshotPath = config.DataDir
 	serfConfig.CoalescePeriod = 3 * time.Second
 	serfConfig.QuiescentPeriod = time.Second
 	serfConfig.UserCoalescePeriod = 3 * time.Second
@@ -53,12 +109,16 @@ func main() {
 
 	shutdownCh := serf.ShutdownCh()
 
-	joined, err := serf.Join(startJoin, false)
+	// We'll always try to join the local agent, since that both tests that
+	// our public IP is working as expected and increases the likelihood
+	// of this initial join succeeding.
+	initialPeers := append(config.InitialPeers, fmt.Sprintf("%s:%d", config.PublicIPAddress, config.GossipPort))
+	joined, err := serf.Join(initialPeers, false)
 	if err != nil {
-		panic(err)
+		log.Printf("Initial join failed: %s", err)
+	} else {
+		log.Printf("Joined a cluster by contacting %d nodes", joined)
 	}
-
-	log.Printf("Joined a cluster by contacting %d nodes", joined)
 
 	for {
 		select {
@@ -74,16 +134,14 @@ func main() {
 
 func PrintMembers(members []serf.Member) {
 	w := tabwriter.NewWriter(os.Stdout, 4, 4, 2, ' ', 0)
-	w.Write([]byte("\nname\taddress\tregion\tdatacenter\tprefix\tstatus\t\n"))
+	w.Write([]byte("\nname\tglobal address\tlocal address\tstatus\t\n"))
 	for _, member := range members {
 		w.Write([]byte(fmt.Sprintf(
-			"%s\t%s:%d\t%s\t%s\t%s\t%s\t\n",
+			"%s\t%s:%d\t%s\t%s\t\n",
 			member.Name,
 			member.Addr,
 			member.Port,
-			member.Tags["region"],
-			member.Tags["datacenter"],
-			member.Tags["prefix"],
+			member.Tags["int_ip"],
 			member.Status,
 		)))
 	}
